6dc11ab0a160f21dd3fd2146edba33282886f68c
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2021-03-15 10:54:48 +0200

    bongo

---
+++ developers.js
@@ -0,0 +1,6 @@
+function setNewImage(){
+document.getElementById("img1").src ="./images/button_back_to_menu_hover.png"
+}
+function setOldImage(){
+    document.getElementById("img1").src ="./images/button_back_to_menu.png"
+}
